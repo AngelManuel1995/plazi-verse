060dee461a5d5e44b13fc7926875371dee66ec0f
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2020-07-22 23:42:12 -0500

    Including platziverse-db localmodule

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,17 +4,29 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 const backend = {
-  type:'redis',
+  type: 'redis',
   redis,
   return_buffers: true
 }
 const settings = {
-  port:1883,
+  port: 1883,
   backend
 }
 
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
+}
+
 const server = new mosca.Server(settings)
+
+let Agent = null; let Metric = null
 
 server.on('clientConnected', (client) => {
   debug(`Client Connected: ${client.id}`)
@@ -29,8 +41,13 @@ server.on('published', (packet, client) => {
   debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', () => {
-  console.log(`${chalk.green.bold('[platziverse-mqtt]')} server is running`)  
+server.on('ready', async () => {
+  const services = await db(config).catch(handlerFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
+  console.log(Metric)
+  console.log(Agent)
+  console.log(`${chalk.green.bold('[platziverse-mqtt]')} server is running`)
 })
 
 server.on('error', handlerFatalError)
