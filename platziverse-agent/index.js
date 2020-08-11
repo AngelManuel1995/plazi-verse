0cc7725dca8a9c1408bf1ee329a241ee9ffc12dd
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2020-08-10 22:32:08 -0500

    Agent done to send message to mqtt server

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const EventEmitter = require('events')
-// const debug = require('debug')('platziverse:agent')
+const debug = require('debug')('platziverse:agent')
+const os = require('os')
+const { promisify } = require('util')
 const defaults = require('defaults')
 const mqtt = require('mqtt')
 const uuid = require('uuid')
@@ -21,8 +23,17 @@ class PlatziverseAgent extends EventEmitter {
     this._started = false
     this._timer = null
     this._client = null
-    this._agentId = null
+		this._agentId = null
+		this._metrics = new Map()
   }
+
+	addMetric(type, fn){
+		this._metrics.set(type, fn)
+	}
+
+	removeMetric(type, fn){
+		this._metrics.delete(type)
+	}
 
   connect () {
     if (!this._started) {
@@ -36,7 +47,31 @@ class PlatziverseAgent extends EventEmitter {
       this._client.on('connected', () => {
         this._agentId = uuid.v4()
         this.emit('connected', this._agentId)
-        this._timer = setInterval(() => {
+        this._timer = setInterval( async () => {
+					if(this._metrics.size > 0){
+						let message = {
+							agent: {
+								uuid: this._agentId,
+								username: opts.username,
+								name: opts.username,
+								hostname: os.hostname() || 'localhost',
+								pid: process.pid
+							}, 
+							metrics: [],
+							timestamp: new Date().getTime()
+						}
+						for (let [ metric, fn ] of this._metrics) {
+							if(fn.length === 1){
+								fn = promisify(fn)
+							}
+							message.metrics.push({
+								type:metric,
+								value: await Promise.resolve(fn())
+							})
+						}
+						debug('Sending', message)
+						this._client.publish('agent/message', JSON.stringify(message) )
+					}
           this.emit('agent/message', 'this is a message')
         }, opts.interval)
       })
@@ -64,7 +99,8 @@ class PlatziverseAgent extends EventEmitter {
     if (this._started) {
       clearInterval(this._timer)
       this._started = false
-      this.emit('disconnected')
+			this.emit('disconnected', this._agentId)
+			this._client.end()
     }
   }
 }
